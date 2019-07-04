e2d73238223a9652ed19cc090da4456016546b2a
tdomenech <43335451+tdomenech@users.noreply.github.com>
2019-07-04 20:27:36 +0200

    Add files via upload

--- src/main_update.py
+++ src/main_update.py
@@ -21,26 +21,26 @@ from sklearn.metrics import balanced_accuracy_score
 w_loss_n = []
 
 def make_weights_for_balanced_classes(images, nclasses):
-    count = [0] * nclasses #vector amb tants zeros com nombre de classes
-    for item in images: #conta les imatges que hi ha a cada classe
+    count = [0] * nclasses
+    for item in images:
         count[item[1]] += 1
-    weight_per_class = [0.] * nclasses #vector amb tants 0. com nombre de classes
+    weight_per_class = [0.] * nclasses
     N = float(sum(count))
     for i in range(nclasses):
-        weight_per_class[i] = N/float(count[i]) #total/el nombre d'imatges per aquella classe
-    weight = [0] * len(images) #defineix weight com un vector de zeros de longitud nombre d'imatges
+        weight_per_class[i] = N/float(count[i])
+    weight = [0] * len(images)
     for idx, val in enumerate(images):
-        weight[idx] = weight_per_class[val[1]] #aqui fa, per cada imatge, li assigna el weight de la classe
+        weight[idx] = weight_per_class[val[1]]
     return weight
-#
+
 def make_weights_for_loss_function(images, nclasses):
-    count = [0] * nclasses  # vector amb tants zeros com nombre de classes
-    for item in images:  # conta les imatges que hi ha a cada classe
+    count = [0] * nclasses
+    for item in images:
         count[item[1]] += 1
-    weight_per_class = [0.] * nclasses  # vector amb tants 0. com nombre de classes
+    weight_per_class = [0.] * nclasses
     N = float(sum(count))
     for i in range(nclasses):
-        weight_per_class[i] = N / float(count[i])  # total/el nombre d'imatges per aquella classe
+        weight_per_class[i] = N / float(count[i])
     return weight_per_class
 
 data_transforms = {
@@ -48,7 +48,7 @@ data_transforms = {
         transforms.Resize((224, 224)),
         transforms.RandomHorizontalFlip(),
         transforms.RandomVerticalFlip(),
-        transforms.RandomRotation(180),         #multiples de 90, 90, 180, 270
+        transforms.RandomRotation(180),
         transforms.RandomRotation(90),
         transforms.RandomRotation(270),
         transforms.ToTensor()
@@ -57,39 +57,40 @@ data_transforms = {
         transforms.Resize((224, 224)),
         transforms.ToTensor()
     ]),
-} #per normalitzar les imatges.
+}
 
 
-data_dir = '/imatge/tdomenech/work/data/ISIC-images/'
+data_dir = 'images directori'
 
+#To write the loss and accuracy values
 escriptura_loss_train = 'train_loss.txt'
 escriptura_loss_val = 'val_loss.txt'
 escriptura_acc_train = 'train_acc.txt'
 escriptura_acc_val = 'val_acc.txt'
 
 
-train_dataset = datasets.ImageFolder(os.path.join(data_dir, 'train'), data_transforms['train']) #mirar si ho fa random
+train_dataset = datasets.ImageFolder(os.path.join(data_dir, 'train'), data_transforms['train'])
 val_dataset = datasets.ImageFolder(os.path.join(data_dir, 'val'), data_transforms['val'])
 
 print('Classes to index', train_dataset.class_to_idx)
 
-weights_train = make_weights_for_balanced_classes(train_dataset.imgs, len(train_dataset.classes)) #crees els weights per les images i les classes
-weights_train = torch.DoubleTensor(weights_train) #n-dimensional array, és més fàcil per operar que una matriu
-sampler_train = torch.utils.data.sampler.WeightedRandomSampler(weights_train, len(weights_train)) #agafa mostres amb la probabilitat de weights_train
+weights_train = make_weights_for_balanced_classes(train_dataset.imgs, len(train_dataset.classes))
+weights_train = torch.DoubleTensor(weights_train)
+sampler_train = torch.utils.data.sampler.WeightedRandomSampler(weights_train, len(weights_train))
 
-weights_val = make_weights_for_balanced_classes(val_dataset.imgs, len(val_dataset.classes)) #igual pero per val
+weights_val = make_weights_for_balanced_classes(val_dataset.imgs, len(val_dataset.classes))
 weights_val = torch.DoubleTensor(weights_val)
 sampler_val = torch.utils.data.sampler.WeightedRandomSampler(weights_val, len(weights_val))
 
 w_loss = make_weights_for_loss_function(train_dataset.imgs, len(train_dataset.classes))
 w_loss = torch.FloatTensor(w_loss)
 
-#classes no balancejades:
+#no balanced clases:
 #train_dataloader = DataLoader(train_dataset, batch_size=16, num_workers=4, shuffle=True)
 #val_dataloader = DataLoader(val_dataset, batch_size=16, num_workers=4, shuffle=True)
 
-#classes balancejades:
-train_dataloader = DataLoader(train_dataset, batch_size=36, num_workers=4, sampler=sampler_train) #combina un dataset i un sampler. Agafa dades del train set, la mida del batch, el nombre de subprocessos per carregar les dades i el sampler (quines dades s'han d'agafar)
+#balanced clases:
+train_dataloader = DataLoader(train_dataset, batch_size=36, num_workers=4, sampler=sampler_train)
 val_dataloader = DataLoader(val_dataset, batch_size=36, num_workers=4, sampler=sampler_val)
 
 
@@ -99,9 +100,6 @@ dataset_sizes = {'train': len(train_dataset), 'val': len(val_dataset)}
 class_names = train_dataset.classes
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-#model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=5)
-#f = open( 'fitxer_loss_acc_100epochs.txt', 'a+' ) #fitxer de sortida de loss i accuracy
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs):
     since = time.time()
@@ -156,22 +154,21 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs):
                 # statistics
                 running_loss += loss.item() * inputs.size(0)
 
-                running_corrects += torch.sum(preds == labels.data) #casos que les prediccions son iguals que la data
+                running_corrects += torch.sum(preds == labels.data)
 
-                #per la confusion matrix el que li he d'entrar labels i preds, però concatenant:
+
                 data_acc = torch.cat((data_acc, labels.data), 0)
                 preds_acc = torch.cat((preds_acc, preds), 0)
 
             epoch_loss = running_loss / dataset_sizes[phase]
             epoch_acc = running_corrects.double() / dataset_sizes[phase]
 
-            #Calculem l'epoch acc de manera diferent:
+
             epoch_balanced_acc = balanced_accuracy_score(data_acc.cpu(), preds_acc.cpu())
             print()
 
             print('{} Loss: {:.4f} Acc: {:.4f} Balanced_Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc, epoch_balanced_acc))
 
-            #f.write('Epoch {}/{}'.format(epoch, num_epochs - 1) + '\n' + 'Loss: {:.4f} Acc: {:.4f}'.format(epoch_loss, epoch_acc) + '\n')
             if phase == 'train':
                 with open(escriptura_loss_train, 'a+') as f:
                     f.write('{}\n'.format(epoch_loss))
@@ -207,25 +204,24 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs):
     print('Best train Acc: {:4f}'.format(best_acc), 'in epoch', num_epoch_acc)
     # load best model weights
     model.load_state_dict(best_model_wts)
-    #f.close()#tancar el fitxer
+
     return model
 
-model_ft = my_resnet.resnet101(pretrained=True) #carregar un model amb uns weights que ja han fet un train, es descarreguen els weights
-num_ftrs = model_ft.fc.in_features #numero de features
-model_ft.fc = nn.Linear(num_ftrs, 7) #aplica transformacio linear y=Atx+b, nou model de 2 classes, nosaltres ho haurem de canviar a 7 (l'imagen net, per defecte son 1000)
+model_ft = my_resnet.resnet101(pretrained=True)
+num_ftrs = model_ft.fc.in_features
+model_ft.fc = nn.Linear(num_ftrs, 7)
 
 model_ft = model_ft.to(device)
 
-criterion = nn.CrossEntropyLoss(weight=(w_loss.cuda())) #combina nn.LogSoftmax() i nn.NLLLoss() en una sola classe. És útil quan tenim un nombre determinat de classes. És la manera de calcular la loss
+criterion = nn.CrossEntropyLoss(weight=(w_loss.cuda()))
 
 # Observe that all parameters are being optimized
-optimizer_ft = optim.Adam(model_ft.parameters(), lr=1e-6) #s'optimitza amb el mètode d'Adam, amb aquest learning rate que normalment és el que s'ha de provar per veure quin funciona
+optimizer_ft = optim.Adam(model_ft.parameters(), lr=1e-6)
 
-# Decay LR by a factor of 0.1 every 7 epochs
+
 #exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=10, gamma=0.1)
 exp_lr_scheduler = None
 
-model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-                       num_epochs=40) #s'entrena el model amb la resnet18,
+model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=40)
 
 torch.save(model_ft, 'output_model_2018_101.pth')
